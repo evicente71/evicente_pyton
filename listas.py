f973a84c5729e8ff7ded1df65a7b05a5598caaa6
evicente71 <92727754+evicente71@users.noreply.github.com>
2021-10-19 11:20:26 +0200

    Update listas.py

--- listas.py
+++ listas.py
@@ -1,1 +1,11 @@
 # Ejemplo de listas
+
+lista = [1, 2, 3, 4]
+print(lista)
+
+#También se puede crear usando list y pasando un objeto iterable.
+lista = list("1234")
+print(lista)
+
+#Una lista sea crea con [] separando sus elementos con comas ,. Una gran ventaja es que pueden almacenar tipos de datos distintos.
+lista = [1, "Hola", 3.67, [1, 2, 3]]
