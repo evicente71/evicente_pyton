3a1eef14ce9ead7338ffed9ef8322b9f700c5e88
evicente71 <92727754+evicente71@users.noreply.github.com>
2021-10-19 14:58:40 +0200

    Update tuplas.py

--- tuplas.py
+++ tuplas.py
@@ -1,1 +1,15 @@
 # Ejemplo de tuplas
+
+tupla = 1, 2, ('a', 'b'), 3
+print(tupla)       
+print(tupla[2][0]) 
+
+#también es posible convertir una lista en tupla haciendo uso de al función tuple()
+lista = [1, 2, 3]
+tupla = tuple(lista)
+print(type(tupla)) 
+print(tupla)   
+
+#se puede también asignar el valor de una tupla con n elementos a n variables.
+x, y, z = tupla
+print(x, y, z) 
