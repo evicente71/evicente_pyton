a4695fa4ef0299e2b3956c3bd156ce52bb9091e2
evicente71 <92727754+evicente71@users.noreply.github.com>
2021-10-19 15:18:25 +0200

    Update fechas.py

--- fechas.py
+++ fechas.py
@@ -12,3 +12,18 @@ now = datetime.now()
 
 print(today)
 print(now)
+
+#Date
+print("El día actual es {}".format(today.day))
+print("El mes actual es {}".format(today.month))
+print("El año actual es {}".format(today.year))
+
+
+#Datetime
+print("El día actual es {}".format(now.day))
+print("El mes actual es {}".format(now.month))
+print("El año actual es {}".format(now.year))
+
+print("La hora actual es {}".format(now.hour))
+print("El minuto actual es {}".format(now.minute))
+print("El segundo actual es {}".format(now.second))
