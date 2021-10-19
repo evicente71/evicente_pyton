ec99ca829380ea43ed9135335e796f90ec7c753e
evicente71 <92727754+evicente71@users.noreply.github.com>
2021-10-19 15:08:53 +0200

    Update conjuntos.py

--- conjuntos.py
+++ conjuntos.py
@@ -1,1 +1,11 @@
 # Ejemplo de conjuntos
+
+s = set([5, 4, 6, 8, 8, 1])
+print(s)       
+print(type(s)) 
+
+s = {5, 4, 6, 8, 8, 1}
+print(len(s))
+
+lista = ["España","Perú", "Argentina"]
+print("España" in lista)
