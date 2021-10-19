df64d36fc4cc3fedec4c14ddc9ba115e3d9339c1
evicente71 <92727754+evicente71@users.noreply.github.com>
2021-10-19 15:14:03 +0200

    Update diccionarios.py

--- diccionarios.py
+++ diccionarios.py
@@ -1,1 +1,22 @@
 # Ejemplo de diccionarios
+
+persona = {
+  "Nombre": "Sara",
+  "Edad": 27,
+  "Documento": 1003882
+}
+print(persona)
+print(persona['Nombre'])  
+print(persona.get('Nombre')) 
+
+#Si el key al que accedemos no existe, se añade automáticamente.
+persona['Direccion'] = "Calle 123"
+print(persona)
+
+#otra forma de definir
+d2 = dict([
+      ('Nombre', 'Pedro'),
+      ('Edad', 32),
+      ('Documento', 1003486),
+])
+print(d2)
