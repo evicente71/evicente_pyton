64ff0f3ca5d2efa443fc15acfc287613b721c5c2
evicente71 <92727754+evicente71@users.noreply.github.com>
2021-10-19 15:17:09 +0200

    Update fechas.py

--- fechas.py
+++ fechas.py
@@ -1,1 +1,14 @@
 # Ejemplo de fechas
+
+#Es necesario importar las depencendias necesarias
+from datetime import date
+from datetime import datetime
+
+#Día actual
+today = date.today()
+
+#Fecha actual
+now = datetime.now()
+
+print(today)
+print(now)
