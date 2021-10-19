daebd79a54cb465050a606527593e54826e1e964
evicente71 <92727754+evicente71@users.noreply.github.com>
2021-10-19 15:02:58 +0200

    Update enteros.py

--- enteros.py
+++ enteros.py
@@ -1,1 +1,17 @@
 # Ejemplo de enteros
+
+i = 12
+print(i)
+print(type(i))
+
+#también es posible asignar valores en binario, hexadecimal y octal
+a = 0b100
+b = 0x17
+c = 0o720
+print(a, type(a)) 
+print(b, type(b)) 
+print(c, type(c)) 
+
+#Es posible convertir a int otro tipo
+b = int(1.6)
+print(b)
